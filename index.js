dbe6cba54c671ee961d18e63364a3654a0158564
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-01 22:55:10 -0500

    First test passed, but second test is unable to access localhost for some reason

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+  const userInfo = {
+    name: name,
+    email: email
+  }
+
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(userInfo)
+  })
+    .then(res => res.json())
+    .then((data) => {
+      const p = document.createElement('p')
+      p.innerText = data.id
+      document.body.append(p)
+    })
+    .catch(err => console.log(err.message))
+}
